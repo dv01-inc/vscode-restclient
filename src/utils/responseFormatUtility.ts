5d3f112de3e1effe17a07667e29ee2a3847ccaeb
Yehuda Graber <yehuda@dv01.co>
2018-08-15 14:10:26 -0400

    Use 'pretty-data' to format JSON, 'jsonc-parser' is very slow with many JSON composed of many small objects.

--- src/utils/responseFormatUtility.ts
+++ src/utils/responseFormatUtility.ts
@@ -1,7 +1,5 @@
 'use strict';
 
-import { applyEdits, format as JSONFormat } from "jsonc-parser/lib/umd/main";
-import { EOL } from "os";
 import { window } from 'vscode';
 import { MimeUtility } from './mimeUtility';
 const pd = require('pretty-data').pd;
@@ -10,11 +8,12 @@ export class ResponseFormatUtility {
     public static formatBody(body: string, contentType: string, suppressValidation: boolean): string {
         if (contentType) {
             if (MimeUtility.isJSON(contentType)) {
-                if (ResponseFormatUtility.IsJsonString(body)) {
-                    const edits = JSONFormat(body, undefined, { tabSize: 2, insertSpaces: true, eol: EOL });
-                    body = applyEdits(body, edits);
-                } else if (!suppressValidation) {
-                    window.showWarningMessage('The content type of response is application/json, while response body is not a valid json string');
+                try {
+                    body = pd.json(body)
+                } catch {
+                    if (!suppressValidation) {
+                        window.showWarningMessage('The content type of response is application/json, while response body is not a valid json string');
+                    }
                 }
             } else if (MimeUtility.isXml(contentType)) {
                 body = pd.xml(body);
@@ -24,14 +23,5 @@ export class ResponseFormatUtility {
         }
 
         return body;
-    }
-
-    public static IsJsonString(data: string) {
-        try {
-            JSON.parse(data);
-            return true;
-        } catch {
-            return false;
-        }
     }
 }
